b33dd8b5ecdc1d029bc028592f1ca3c9501f5474
Eric Stanulis <erstanulis@gmail.com>
2017-10-28 17:24:02 -0500

    Started building Handlebars templates

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+
+select: function(callback){
+    orm.selectWhere(),
+insert: orm.insertOne(),
+update: orm.updateOne()
+};
+
+module.exports = burger;
